493ff88e1747e9a96953e71a9e9d8e96f8b774aa
ZPotts <zack.potts@gmail.com>
2023-02-07 16:55:56 -0700

    fix feed items and it's working and I'm so damn happy you woldn't believe it, turns out Next Image component won't allow for dynamic sizing, and I couldn't sleep last night trying to figure it out. read the docs people.

--- components/home/feedItem.tsx
+++ components/home/feedItem.tsx
@@ -5,15 +5,20 @@ import styles from '@/styles/modules/Home.module.css';
 function feedItem(){
     return(
         <div className={styles.feedItem} data-testid="feedItem">
+            {/* 
             <Image 
                 fill
                 alt="Test Image"
                 src="/img/article/test.webp" 
-                className="feedItemImg"
+                className={styles.feedItemImg}
+            />s
+            */}
+            <img src="/img/article/test.webp"
+                className={styles.feedItemImg}
             />
-            <div className="headline">Title</div>
-            <div className="description">Description</div>
-            <div className="byline">ByLine</div>
+            <div className={styles.feedItemCategory}>Category</div>
+            <div className={styles.feedItemHeadline}>Hrere is the title, and I hope that you like it. </div>
+            <div className={styles.feedItemDescription}>Hrere is the scription, and I hope that you like it. Let's make it even longer and see how we feel about that. How about this for a description blurby.</div>
         </div>
     )
 }
